e47d4dd699c65d934aeb99630af37b26423108b7
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-30 01:31:23 +0530

    add: product bulk add , update and delete

--- src/main/java/com/avijeet/nykaa/controller/product/ProductController.java
+++ src/main/java/com/avijeet/nykaa/controller/product/ProductController.java
@@ -1,19 +1,19 @@
 package com.avijeet.nykaa.controller.product;
 
 import com.avijeet.nykaa.constants.ApiConstants;
+import com.avijeet.nykaa.dto.product.PageResponse;
 import com.avijeet.nykaa.dto.product.ProductRequestDto;
 import com.avijeet.nykaa.dto.product.ProductResponseDto;
+import com.avijeet.nykaa.dto.product.ProductUpdateDto;
 import com.avijeet.nykaa.service.product.ProductService;
 import com.avijeet.nykaa.utils.api.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +28,38 @@ public class ProductController {
         ProductResponseDto savedProduct = productService.addProduct(productRequestDto);
         ApiResponse<ProductResponseDto> response = ApiResponse.success(ApiConstants.DONE_MESSAGE,savedProduct);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ApiResponse<ProductResponseDto>> addProduct(@Valid @RequestBody ProductUpdateDto productUpdateDto) {
+        log.info("POST /api/v1/products/update - name={}", productUpdateDto.name());
+        ProductResponseDto savedProduct = productService.updateProduct(productUpdateDto);
+        ApiResponse<ProductResponseDto> response = ApiResponse.success(ApiConstants.DONE_MESSAGE,savedProduct);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse<ProductResponseDto>> deleteProduct(@PathVariable Long id) {
+        log.info("DELETE /api/v1/products/delete/{} - Deleting product with id={}", id, id);
+        productService.deleteProduct(id);
+        ApiResponse<ProductResponseDto> response = ApiResponse.success(ApiConstants.DONE_MESSAGE, null);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<PageResponse<ProductResponseDto>>> getAllProducts(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+    ) {
+        PageResponse<ProductResponseDto> paginatedProducts = productService.getAllProducts(pageNo, pageSize, sortBy, sortDir);
+        return ResponseEntity.ok(ApiResponse.success("Products fetched successfully", paginatedProducts));
+    }
+
+    @PostMapping("/addBulk")
+    public ResponseEntity<ApiResponse<List<ProductResponseDto>>> addBulkProducts(@Valid @RequestBody List<ProductRequestDto> requestDtosList) {
+        List<ProductResponseDto> bulkProducts = productService.addBulkProducts(requestDtosList);
+        return ResponseEntity.ok(ApiResponse.success("Products fetched successfully", bulkProducts));
     }
 }
--- src/main/java/com/avijeet/nykaa/service/product/ProductService.java
+++ src/main/java/com/avijeet/nykaa/service/product/ProductService.java
@@ -1,13 +1,24 @@
 package com.avijeet.nykaa.service.product;
 
+import com.avijeet.nykaa.dto.product.PageResponse;
 import com.avijeet.nykaa.dto.product.ProductRequestDto;
 import com.avijeet.nykaa.dto.product.ProductResponseDto;
+import com.avijeet.nykaa.dto.product.ProductUpdateDto;
 import com.avijeet.nykaa.entities.Product;
+import com.avijeet.nykaa.enums.Brand;
+import com.avijeet.nykaa.enums.Category;
+import com.avijeet.nykaa.exception.ProductNotFoundException;
 import com.avijeet.nykaa.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -33,6 +44,76 @@ public class ProductService {
         return mapToResponseDto(savedProduct);
     }
 
+    @Transactional
+    public ProductResponseDto updateProduct(ProductUpdateDto productUpdateDto) {
+        Product existingProduct = productRepository.findById(productUpdateDto.id())
+                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productUpdateDto.id()));
+
+        if(!productUpdateDto.name().isEmpty()) {
+            existingProduct.setName(productUpdateDto.name());
+        }
+
+        if(!productUpdateDto.brand().isEmpty()) {
+            existingProduct.setBrand(Brand.valueOf(productUpdateDto.brand()));
+        }
+
+        if(!productUpdateDto.category().isEmpty()) {
+            existingProduct.setCategory(Category.valueOf(productUpdateDto.category()));
+        }
+
+        if(!productUpdateDto.price().isNaN()) {
+            existingProduct.setPrice(productUpdateDto.price());
+        }
+
+        Product savedProduct = productRepository.save(existingProduct);
+
+        return mapToResponseDto(savedProduct);
+    }
+
+    @Transactional
+    public void deleteProduct(Long productId) {
+        if(!productRepository.existsById(productId)) {
+            throw new ProductNotFoundException("Product not found with id: " + productId);
+        }
+        productRepository.deleteById(productId);
+    }
+
+    public PageResponse<ProductResponseDto> getAllProducts(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Product> productPage = productRepository.findAll(pageable);
+
+        List<ProductResponseDto> content = productPage.getContent().stream()
+                .map(this::mapToResponseDto)
+                .toList();
+
+        return new PageResponse<>(
+                content,
+                productPage.getNumber(),
+                productPage.getSize(),
+                productPage.getTotalElements(),
+                productPage.getTotalPages(),
+                productPage.isLast()
+        );
+    }
+
+    @Transactional
+    public List<ProductResponseDto> addBulkProducts(List<ProductRequestDto> requestDtos) {
+        List<Product> productsToSave = requestDtos.stream()
+                .map(this::mapToEntity)
+                .toList();
+
+        List<Product> savedProducts = productRepository.saveAll(productsToSave);
+
+        return savedProducts.stream()
+                .map(this::mapToResponseDto)
+                .toList();
+    }
+
     private ProductResponseDto mapToResponseDto(Product product) {
         return new ProductResponseDto(
                 product.getId(),
@@ -44,4 +125,14 @@ public class ProductService {
                 product.getUpdatedAt()
         );
     }
+
+    private Product mapToEntity(ProductRequestDto productRequestDto) {
+        return Product.builder()
+                .price(productRequestDto.price())
+                .category(Category.valueOf(String.valueOf(productRequestDto.category())))
+                .brand(Brand.valueOf(String.valueOf(productRequestDto.brand())))
+                .name(productRequestDto.name())
+                .build();
+    }
+
 }
